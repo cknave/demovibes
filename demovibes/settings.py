c2fd4b84677d1bc1eaa6668112378889788b8c28
terrasque@thelazy.net <terrasque@thelazy.net@5733aaf2-ac46-11de-892d-7b2b76e7707e>
2010-07-21 17:49:05 +0000

    New, more general install instructions, list of required python software, and DEFAULT SETTINGS.PY - BEWARE
    git-svn-id: http://demovibes.googlecode.com/svn/branches/jinja_merge@256 5733aaf2-ac46-11de-892d-7b2b76e7707e

---
+++ demovibes/settings.py
@@ -0,0 +1,221 @@
+# Django settings for demovibes project.
+
+import os
+import django
+# calculated paths for django and the site
+# used as starting points for various other paths
+DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__)) 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+SITE_ROOT = os.path.dirname(SITE_ROOT)
+
+def pj(*path):
+    return os.path.join(SITE_ROOT, *path)
+
+from smileys import SMILEYS
+
+DEBUG = True
+TEMPLATE_DEBUG = False
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+HAYSTACK_SITECONF = 'demovibes.search_sites'
+HAYSTACK_SEARCH_ENGINE = "whoosh"
+HAYSTACK_WHOOSH_PATH = pj("local","whoosh")
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = pj('sqlite.db')             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Use fulltext search on song title, artist handle and group name. Only supported on mysql, 
+# and full-text have to be manually added to the DB
+USE_FULLTEXT_SEARCH = False
+
+# Search results per type (group, artist, songs)
+SEARCH_LIMIT = 40
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Oslo'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+# There may be a situation where your server doesn't have a built-in SMTP server, and you
+# Wish to use a 3rd party SMTP server. Uncommenting these boxes and setting them accordingly
+# Will allow this to take place. The default SMTP options may not deliver to outside world, in
+# Which case, use this instead. AAK.
+
+# Host for sending e-mail. This would be the SMTP host for your email provider
+#EMAIL_HOST = ''
+#DEFAULT_FROM_EMAIL = ""
+
+# Port for sending e-mail. By default, it is almost always 25.
+#EMAIL_PORT = 25
+
+# Optional SMTP authentication information for EMAIL_HOST. Some SMTP servers require the
+# Authoriazation to be the same as the email address for the count, and work better using
+# The + chanarcter instead of @.
+#EMAIL_HOST_USER = 'smtpuser+domain.com'
+#EMAIL_HOST_PASSWORD = 'smtp_password'
+#EMAIL_USE_TLS = False
+
+# END SMTP Configuration
+
+# Customize the dimensions on the avatars used on the site. AVATAR_SIZE is a value in
+# Bytes that the image file cannot exceed. Reccomend no less than 40Kb so users can have
+# Some pretty AnimGIF files to use. HEIGHT and WIDTH are specified in pixels.
+MAX_AVATAR_SIZE = 65536
+MAX_AVATAR_HEIGHT = 100
+MAX_AVATAR_WIDTH = 100
+
+# Artist pictures can also be customized to fit your site using these values
+MAX_ARTIST_AVATAR_SIZE = 65536
+MAX_ARTIST_AVATAR_HEIGHT = 200
+MAX_ARTIST_AVATAR_WIDTH = 200
+
+# Group Logo controls
+MAX_GROUP_AVATAR_SIZE = 65536
+MAX_GROUP_AVATAR_HEIGHT = 250
+MAX_GROUP_AVATAR_WIDTH = 250
+
+# Max internal length of the oneliner, in characters
+MAX_ONELINER_LENGTH = 256
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = DOCUMENT_ROOT = pj('static')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/static/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Please set a backend here
+# We recommend memcached.
+# If that's not avaliable we recommend file or database cache,
+# because locmem can't always be trusted to have a consistent cache
+#CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+CACHE_BACKEND = 'dummy://'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'replaceThis!'
+
+AUTH_PROFILE_MODULE = 'webview.userprofile'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+ #add ?profiler to url to get a profile of the page. Debug needs to be on
+)
+
+ROOT_URLCONF = 'demovibes.urls'
+
+# New changes introduced to demovibes to support user/global template changes. The
+# Local folder should always be checked first for user customized templates. In the
+# Event that this fails, then the global template is used instead. AAK.
+TEMPLATE_DIRS = (
+    pj('templates', 'local'),
+    pj('templates', 'global'),
+    pj('templates', 'jinja', 'global'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+JINJA2_TEMPLATE_DIRS = (
+    pj('templates', 'jinja', 'global'),
+)
+
+# Maximum number of songs a user can have in the queue at the same time.
+SONGS_IN_QUEUE = 5
+
+# Time to lock a song until it can be requested again.
+SONG_LOCK_TIME = { 'days' : 0, 'hours' : 0, 'minutes' : 5 }
+
+# Need to have at least one song marked as jingle for this to work
+# Will play one every 30 minutes or 10 songs, but not more often than every 20 minutes.
+PLAY_JINGLES = False
+
+# How many objects per page:
+PAGINATE = 30
+FORUM_PAGINATE = 15
+
+# When displaying approved songs, specify the maximum number to display on the page here.
+# You should take into account that you will want to display about an average of 2 days worth
+# Of uploads; Otherwise the list will never be useful except at the time a new batch of
+# Large songs are approved. AAK
+UPLOADED_SONG_COUNT = 150
+
+# You can specify if you want shortened url's in the oneliner. If enabled, it will take
+# A link such as http://www.blah.com/blah/bleh/blob.html and make a clickable link to
+# Only http://www.blah.com in the display portion, the clicked link contains the full path.
+# Set to a value of 1 to enable truncated links.
+SHORTEN_ONELINER_LINKS = 0
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",    
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'demovibes.webview',
+    'demovibes.registration',
+    'demovibes.forum',
+    'django.contrib.markup',
+    'south',
+    'tagging',
+    'django_extensions',
+    'haystack',
+    'openid_provider',
+)
+
+try:
+    from settings_local import *
+except:
+    pass
+
+try:
+    modify_globals(globals())
+except:
+    pass
