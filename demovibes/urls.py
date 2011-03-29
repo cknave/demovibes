7b741ca3fa55f44dfe3b1e7fdad04ebaec9696f4
terrasque@thelazy.net <terrasque@thelazy.net@5733aaf2-ac46-11de-892d-7b2b76e7707e>
2011-03-29 11:22:03 +0000

    Django 1.3 admin fix
    git-svn-id: http://demovibes.googlecode.com/svn/branches/jinja_merge@437 5733aaf2-ac46-11de-892d-7b2b76e7707e

--- demovibes/urls.py
+++ demovibes/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('demovibes.registration.urls')),
     (r'^forum/', include('forum.urls')),
 
