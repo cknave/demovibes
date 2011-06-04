a0bca289fb8a69bba690ce06e94ad8db3bb525ba
TheTerrasque@gmail.com <TheTerrasque@gmail.com@5733aaf2-ac46-11de-892d-7b2b76e7707e>
2011-06-04 09:55:43 +0000

    Some more fixes - static paths in JS, moved new compilation CSS and JS to external files
    git-svn-id: http://demovibes.googlecode.com/svn/branches/jinja_merge@507 5733aaf2-ac46-11de-892d-7b2b76e7707e

--- static/js/bbedit.js
+++ static/js/bbedit.js
@@ -1,19 +1,19 @@
 var bbOptions = {
     "buttons": [
-        {"name": "Bold", "button": "/static/explicit.gif", "content": "[b]%s[/b]", "description": "Marks text as <strong>bold</strong>"},
-        {"name": "Italic", "button": "/static/page_white_edit.png", "content": "[i]%s[/i]", "description": "Marks text as <i>italic</i>"},
-        {"name": "Big", "button": "/static/add.png", "content": "[big]%s[/big]", "description": "Marks text as big"},
-        {"name": "Small", "button": "/static/find.png", "content": "[small]%s[/small]", "description": "Marks text as small"},
-        {"name": "Code tag", "button": "/static/comment.png", "content": "[code]%s[/code]", "description": "Marks text as code"},
-        {"name": "Quote", "button": "/static/script.png", "content": "[quote]%s[/quote]", "description": "Marks text as quote"},
-        {"name": "YouTube link", "button": "/static/youtube_icon.png", "content": "[yt]%s[/yt]", "description": "Creates a link to a YouTube video <br/>Use video ID that is after v= in the URL"},
-        {"name": "HTTP Url", "button": "/static/world_link.png", "content": "[url]%s[/url]", "description": "Creates a web link. <br/>Link MUST start with http or ftp"},
-        {"name": "Image embed", "button": "/static/icon_screenshot.png", "content": "[img]%s[/img]", "description": "Embeds an image into the page"},
-        {"name": "Song link", "button": "/static/music.png", "content": "[song]%s[/song]", "description": "Creates a link to given song ID"},
-        {"name": "Song queue link", "button": "/static/control_play.png", "content": "[queue]%s[/queue]", "description": "Creates a link to given song ID, with queue button"},
-        {"name": "User link", "button": "/static/user.png", "content": "[user]%s[/user]", "description": "Creates a link to given user"},
-        {"name": "Artist link", "button": "/static/user_green.png", "content": "[artist]%s[/artist]", "description": "Creates a link to given artist ID"},
-        {"name": "Thread link", "button": "/static/newspaper.png", "content": "[thread]%s[/thread]", "description": "Creates a link to given thread ID"}
+        {"name": "Bold", "button": "explicit.gif", "content": "[b]%s[/b]", "description": "Marks text as <strong>bold</strong>"},
+        {"name": "Italic", "button": "page_white_edit.png", "content": "[i]%s[/i]", "description": "Marks text as <i>italic</i>"},
+        {"name": "Big", "button": "add.png", "content": "[big]%s[/big]", "description": "Marks text as big"},
+        {"name": "Small", "button": "find.png", "content": "[small]%s[/small]", "description": "Marks text as small"},
+        {"name": "Code tag", "button": "comment.png", "content": "[code]%s[/code]", "description": "Marks text as code"},
+        {"name": "Quote", "button": "script.png", "content": "[quote]%s[/quote]", "description": "Marks text as quote"},
+        {"name": "YouTube link", "button": "youtube_icon.png", "content": "[yt]%s[/yt]", "description": "Creates a link to a YouTube video <br/>Use video ID that is after v= in the URL"},
+        {"name": "HTTP Url", "button": "world_link.png", "content": "[url]%s[/url]", "description": "Creates a web link. <br/>Link MUST start with http or ftp"},
+        {"name": "Image embed", "button": "icon_screenshot.png", "content": "[img]%s[/img]", "description": "Embeds an image into the page"},
+        {"name": "Song link", "button": "music.png", "content": "[song]%s[/song]", "description": "Creates a link to given song ID"},
+        {"name": "Song queue link", "button": "control_play.png", "content": "[queue]%s[/queue]", "description": "Creates a link to given song ID, with queue button"},
+        {"name": "User link", "button": "user.png", "content": "[user]%s[/user]", "description": "Creates a link to given user"},
+        {"name": "Artist link", "button": "user_green.png", "content": "[artist]%s[/artist]", "description": "Creates a link to given artist ID"},
+        {"name": "Thread link", "button": "newspaper.png", "content": "[thread]%s[/thread]", "description": "Creates a link to given thread ID"}
     ]
 };
 
@@ -23,7 +23,7 @@ function createField(element) {
     var target = editarea.parent();
     var editbar = $("<div/>").addClass("editbar");
     $.each(bbOptions.buttons, function (i, b) {
-        var button = $("<img/>").attr("src", b.button).addClass("editButton");
+        var button = $("<img/>").attr("src", STATIC_URL + b.button).addClass("editButton");
         button.qtip({
             "content": {
                 "text": b.description,
