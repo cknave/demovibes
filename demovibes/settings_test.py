9ef908645f51fb6917fed1824ae8103d0f3e430d
Mikael Turoy <demovibes@thelazy.net>
2011-10-11 10:50:36 +0200

    Fix for some browsers not sending 'input type img' value

--- demovibes/settings_test.py
+++ demovibes/settings_test.py
@@ -11,6 +11,7 @@ MEDIA_URL = 'http://www.scenemusic.net/static/'
 
 CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 
+KEY_PREFIX = "TT"
 #CACHES = {
 #    'default': {
 #        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
