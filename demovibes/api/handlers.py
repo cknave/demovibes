afca630f1460c0c5568ea4598a28a264a1d12ef0
Mikael Turoy <demovibes@thelazy.net>
2011-10-23 11:25:56 +0200

    More api stuff

--- demovibes/api/handlers.py
+++ demovibes/api/handlers.py
@@ -1,8 +1,9 @@
 from piston.handler import BaseHandler, AnonymousBaseHandler
-from webview.models import Song, Queue, User, Artist
+from webview.models import Song, Queue, User, Artist, Userprofile
 from django import forms
 from webview import common
-from piston.utils import validate
+from webview import forms as webforms
+from piston.utils import validate, rc
 
 class SongHandler(BaseHandler):
     """
@@ -89,22 +90,20 @@ class BaseQueueHandler(object):
     allowed_methods = ('GET',)
     model = Queue
 
-    def read(self, request, queueid=None):
+    def read(self, request):
         """
         Get 20 latest songs in queue
         """
-        if queueid:
-            return Queue.objects.get(id=queueid)
         return Queue.objects.all()[:20]
 
     @staticmethod
     def resource_uri():
-        return ('api_queue_handler', ['id'])
+        return ('api_queue_handler2', [])
 
 class AnonQueueHandler(BaseQueueHandler, AnonymousBaseHandler):
     pass
 
-class QueueHandler(BaseQueueHandler, BaseHandler):
+class AuthQueueHandler(BaseQueueHandler, BaseHandler):
     """
     For handling the play queue
     """
@@ -132,7 +131,10 @@ class UserBaseHandler(object):
         """
         Return user information
         """
-        return User.objects.get(username=username)
+        user = User.objects.get(username=username)
+        if user.get_profile().viewable_by(request.user):
+            return user
+        return rc.FORBIDDEN
 
     @staticmethod
     def resource_uri():
@@ -141,6 +143,33 @@ class UserBaseHandler(object):
 class AnonUserHandler(UserBaseHandler, AnonymousBaseHandler):
     pass
 
-class UserHandler(UserBaseHandler, BaseHandler):
+class AuthUserHandler(UserBaseHandler, BaseHandler):
     anonymous = AnonUserHandler
-    pass
+
+class AuthProfile(BaseHandler):
+    """
+    Read and modify authenticated user's profile
+    """
+    allowed_methods = ('GET', 'PUT')
+    model = Userprofile
+    fields = webforms.ProfileForm.Meta.fields
+
+    def read(self, request):
+        """
+        Show profile of authenticated user
+        """
+        return request.user.get_profile()
+
+    @validate(webforms.ProfileForm)
+    def update(self, request):
+        """
+        Update profile of authenticated user
+        """
+        userp = request.user.get_profile()
+        profile = webforms.ProfileForm(request.PUT, instance=userp)
+        profile.save()
+        return rc.ALL_OK
+
+    @staticmethod
+    def resource_uri():
+        return ('api_profile_handler', [])
--- demovibes/api/urls.py
+++ demovibes/api/urls.py
@@ -1,22 +1,24 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from api.handlers import SongHandler, QueueHandler, UserHandler, ArtistHandler
+from api.handlers import SongHandler, AuthQueueHandler, AuthUserHandler, ArtistHandler, AuthProfile
 from piston.doc import documentation_view
 from piston.authentication import HttpBasicAuthentication
 
 auth = HttpBasicAuthentication(realm="My Realm")
 ad = { 'authentication': auth }
 
+profile_handler = Resource(AuthProfile, **ad)
 song_handler = Resource(SongHandler)
-queue_handler = Resource(QueueHandler, **ad)
-user_handler = Resource(UserHandler, **ad)
+queue_handler = Resource(AuthQueueHandler, **ad)
+user_handler = Resource(AuthUserHandler, **ad)
 artist_handler = Resource(ArtistHandler)
 
 urlpatterns = patterns('',
    url(r'^song/(?P<song_id>\d+)/', song_handler, name="api_song_handler"),
    url(r'^artist/(?P<artist_id>\d+)/', artist_handler, name="api_artist_handler"),
    url(r'^queue/', queue_handler, name="api_queue_handler2"),
+   url(r'^profile/', profile_handler, name="api_profile_handler"),
    url(r'^queue/(?P<queueid>\d+)/', queue_handler, name="api_queue_handler"),
    url(r'^user/(?P<username>\w+)/', user_handler, name="api_user_handler"),
-   url(r'^^doc/$', documentation_view, name="api_docs"),
+   url(r'^$', documentation_view, name="api_docs"),
 )
