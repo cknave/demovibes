fea378796c682ec4c6afea15a3efc10da0216e6d
Mikael Turoy <demovibes@thelazy.net>
2011-10-24 23:59:08 +0200

    Fix url regex not parsing to eol

--- demovibes/api/urls.py
+++ demovibes/api/urls.py
@@ -16,13 +16,13 @@ artist_handler = Resource(ArtistHandler)
 votes_handler = Resource(AuthSongVotesHandler, **ad)
 
 urlpatterns = patterns('',
-   url(r'^song/(?P<song_id>\d+)/', song_handler, name="api_song_handler"),
-   url(r'^song/(?P<song_id>\d+)/votes/', votes_handler, name="api_votes_handler"),
-   url(r'^artist/(?P<artist_id>\d+)/', artist_handler, name="api_artist_handler"),
-   url(r'^queue/', queue_handler, name="api_queue_handler2"),
-   url(r'^profile/', profile_handler, name="api_profile_handler"),
-   url(r'^queue/(?P<queueid>\d+)/', queue_handler, name="api_queue_handler"),
-   url(r'^user/(?P<username>\w+)/', user_handler, name="api_user_handler"),
-   url(r'^oneliner/', oneliner_handler, name="api_oneliner_handler"),
+   url(r'^song/(?P<song_id>\d+)/$', song_handler, name="api_song_handler"),
+   url(r'^song/(?P<song_id>\d+)/votes/$', votes_handler, name="api_votes_handler"),
+   url(r'^artist/(?P<artist_id>\d+)/$', artist_handler, name="api_artist_handler"),
+   url(r'^queue/$', queue_handler, name="api_queue_handler2"),
+   url(r'^profile/$', profile_handler, name="api_profile_handler"),
+   url(r'^queue/(?P<queueid>\d+)/$', queue_handler, name="api_queue_handler"),
+   url(r'^user/(?P<username>\w+)/$', user_handler, name="api_user_handler"),
+   url(r'^oneliner/$', oneliner_handler, name="api_oneliner_handler"),
    url(r'^$', documentation_view, name="api_docs"),
 )
